4b6e776f8b8a3ecaf9cd2e0796290f7336edca51
RISHI <rishiagarwal95@gmail.com>
2017-05-25 12:40:33 +0530

    code modified

--- client/app.js
+++ client/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './components/ItemList';
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
-import { baseUrl } from './components/common-components/common';
 import Main from './components/Main.component';
 import UserList from './components/UserList';
 import DetailComponent from './components/DetailComponent';
